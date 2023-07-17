b50b6a8f15bc8ac09db23e71cd42eaad00058ae0
Federico Ruttkay <fede.ruttkay@gmail.com>
2023-07-17 14:46:18 -0300

    Revision de codigo en general

--- main.js
+++ main.js
@@ -3,18 +3,16 @@ const inputField = document.getElementById("input-field");
 const generateBtn = document.getElementById("generate-btn");
 const dotsField = document.querySelector(".dots-field");
 
-body.addEventListener("click", clickHandle);
-inputField.addEventListener("keydown", enterHandle);
+body.addEventListener("click", clickAndEnter);
+inputField.addEventListener("keydown", clickAndEnter);
 
-function clickHandle(event) {
-	if (event.target === generateBtn) {
-		generateDots();
-	}
-}
-
-function enterHandle(event) {
-	if (event.key === "Enter") {
-		generateDots();
+function clickAndEnter(event) {
+	if (event.target === generateBtn || event.key === "Enter") {
+		try {
+			generateDots();
+		} catch (e) {
+			alert(e); // Hacer algo mejor
+		}
 	}
 }
 
@@ -22,8 +20,9 @@ function generateDots() {
 	const inputValue = inputField.value;
 
 	if (inputValue >= 0 && inputValue <= 1000) numberOfDots = inputValue;
-	else numberOfDots = 0;
-
+	else {
+		throw new Error("Number of dots must be between 0 and 1000");
+	}
 	let dotsCode = "";
 
 	for (let i = 1; i <= numberOfDots; i++) {
