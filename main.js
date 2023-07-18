38c4b1b9d99ee6f69d6c016714c51a08768c6819
Federico Ruttkay <fede.ruttkay@gmail.com>
2023-07-17 21:02:46 -0300

    Mejorado el responsive

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ const inputField = document.getElementById("input-field");
 const generateBtn = document.getElementById("generate-btn");
 const dotsField = document.querySelector(".dots-field");
 
+const dotsFieldStyles = window.getComputedStyle(dotsField);
+const fieldWidth = dotsFieldStyles.getPropertyValue("width");
+
+dotsField.style.height = fieldWidth;
+
 body.addEventListener("click", clickAndEnter);
 inputField.addEventListener("keydown", clickAndEnter);
 
@@ -19,6 +24,12 @@ function clickAndEnter(event) {
 function generateDots() {
 	const inputValue = inputField.value;
 
+	// const availableField = (100 * (fieldWidth - 5)) / fieldWidth;
+	const availableField =
+		(100 * (fieldWidth.replace(/px/, "") - 5)) / fieldWidth.replace(/px/, "");
+
+	console.log(availableField);
+
 	if (inputValue >= 0 && inputValue <= 1000) numberOfDots = inputValue;
 	else {
 		throw new Error("Number of dots must be between 0 and 1000");
@@ -27,8 +38,8 @@ function generateDots() {
 
 	for (let i = 1; i <= numberOfDots; i++) {
 		dotsCode += `<div class="dot" style="--top: ${
-			Math.random() * 99
-		}%; --left: ${Math.random() * 99}%"></div>`;
+			Math.random() * availableField
+		}%; --left: ${Math.random() * availableField}%"></div>`;
 	}
 
 	dotsField.innerHTML = dotsCode;
